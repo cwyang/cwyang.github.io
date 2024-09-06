0081233a65ca0542a6819f5c1e4e544b5c9979cf
Chul-Woong Yang <cwyang@gmail.com>
2024-09-07 00:02:27 +0900

    deploy.sh

--- hugo/deploy.sh
+++ hugo/deploy.sh
@@ -2,32 +2,22 @@
 
 # Exit immediately if a command exits with a non-zero status
 set -e
+current_time=$(date -u +"Update %Y-%m-%dT%H:%M:%S.%3NZ")
 
 # Define variables
-REPO="https://github.com/cwyang/cwyang.github.io"
 BRANCH="master" # Change this to your target branch
 PUBLIC_DIR="public"
 
-# Build the Hugo site
+rm -rf $PUBLIC_DIR
+git worktree add $PUBLIC_DIR $BRANCH
+
 hugo
 
-# Navigate to the public directory
 cd $PUBLIC_DIR
-
-# Initialize a new Git repository in the public directory
-git init
-
-# Add remote repository
-git remote add origin $REPO
-
-# Add all changes to git
 git add .
-
-# Commit changes
-git commit -m "Deploy site"
-
-# Push changes to the specified branch
-git push -f origin $BRANCH
-
-# Return to the root of the project
+git commit -m "$current_time"
+git push origin $BRANCH
 cd ..
+
+# Remove the worktree
+#git worktreerremover$PUBLIC_DIR
